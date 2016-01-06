c138a1b69bebcf649ea78faade0ad85ce078427d
Kyran Magpantay <magpantay.kyran@gmail.com>
2016-01-06 15:10:20 -0800

    Complete overhaul
    Restarted the project from scratch, and ended up with a fully working program that fulfilled the requirements.

--- DataAnalyze.java
+++ DataAnalyze.java
@@ -2,37 +2,32 @@ package project2Package;
 
 import java.util.Scanner;
 
-public class Project2 {
+public class Project2Trials {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 
 		System.out.print("Please enter the sample size: ");
 		int sampleSize = in.nextInt();
+		checkIntInput(sampleSize);
 
-		int[] numInputData = new int[(sampleSize * 4)];
+		int trialAmount = 4;
 		int[] dataT0 = new int[sampleSize];
 		int[] dataT1 = new int[sampleSize];
 		int[] dataT2 = new int[sampleSize];
 		int[] dataT3 = new int[sampleSize];
-		int trialAmount = 4;
-		int[] averagesSum = new int[trialAmount];
-		int[] averages = new int[trialAmount];
-		int minimum = 0;
-		int maximum = 0;
 
 		for(int i = 0; i < trialAmount; i++){
 			System.out.println("Enter numbers for Trial " + i);
 			for (int j = 0; j < sampleSize; j++){
 				System.out.print("Enter sample #" + j + ": ");
-				if(i == 0){
+				if (i == 0){
 					dataT0[j] = in.nextInt();
 				}
-				else if(i == 1){
+				else if (i == 1){
 					dataT1[j] = in.nextInt();
 				}
-				else if(i == 2){
+				else if (i == 2){
 					dataT2[j] = in.nextInt();
 				}
 				else{
@@ -41,51 +36,62 @@ public class Project2 {
 			}
 		}
 
-		System.out.println("Sample # \t Trial 1 \t Trial 2 \t Trial3 \t Trial 4");
-		for(int i = 0; i < sampleSize; i++){
+		printList(sampleSize, dataT0, dataT1, dataT2, dataT3);
+
+		int sum[] = findSum(trialAmount, sampleSize, dataT0, dataT1, dataT2, dataT3);
+		int averages[] = findAverages(trialAmount, sum, sampleSize);
+
+		extremes(averages);
+
+		in.close();
+	}
+
+	public static void checkIntInput(int nextInt){
+		if (nextInt <= 0){
+			System.out.println("There's no need for this then!");
+			System.exit(1);
+		}
+	}
+
+	public static void printList(int sampleSize, int[] dataT0, int[] dataT1, int[] dataT2, int[] dataT3){
+		System.out.println("Sample # \t Trial 1 \t Trial 2 \t Trial 3 \t Trial 4");
+		for (int i = 0; i < sampleSize; i++){
 			System.out.print(i + "\t");
-			for(int j = 0; j < 4; j++){
-				System.out.print("\t" + numInputData[j] + "\t");
-				if(j % 3 == 0 && j != 0){
-					System.out.println("");
-				}
-			}
+			System.out.print("\t" + dataT0[i] + "\t");
+			System.out.print("\t" + dataT1[i] + "\t");
+			System.out.print("\t" + dataT2[i] + "\t");
+			System.out.print("\t" + dataT3[i] + "\t");
+			System.out.println("");
 		}
 
 		System.out.println("--------------------------------------------------------------------------------------------");
+	}
 
-		int count = 0;
-
-		for(int i = 0, index = 0; count < sampleSize; i++){
-			if (index == 0){
-				averagesSum[index] += numInputData[i];
-				index++;
-			}
-			else if(index == 1){
-				averagesSum[index] += numInputData[i];
-				index++;
-			}
-			else if(index == 2){
-				averagesSum[index] += numInputData[i];
-				index++;
-			}
-			else{
-				averagesSum[index] += numInputData[i];
-				index = 0;
-				count++;
-			}
+	public static int[] findSum(int trialAmount, int sampleSize, int[] dataT0, int[] dataT1, int[] dataT2, int[] dataT3){
+		int[] sum = new int [trialAmount];
+		for (int i = 0; i < sampleSize; i++){
+			sum[0] += dataT0[i];
+			sum[1] += dataT1[i];
+			sum[2] += dataT2[i];
+			sum[3] += dataT3[i];
 		}
+		return sum;
+	}
 
-		for (int index = 0; index < averages.length; index++){
-			averages[index] = (averagesSum[index] / sampleSize);
-		}
+	public static int[] findAverages(int trialAmount, int[] sum, int sampleSize){
+		int[] averages = new int[trialAmount];
+		averages[0] = (sum[0] / sampleSize);
+		averages[1] = (sum[1] / sampleSize);
+		averages[2] = (sum[2] / sampleSize);
+		averages[3] = (sum[3] / sampleSize);
+		return averages;
+	}
 
-		maximum = averages[0];
-		minimum = averages[0];
-
-		System.out.print("\t");
-
-		for(int i = 0; i < averages.length; i++){
+	public static void extremes(int[] averages){
+		int minimum = averages[0];
+		int maximum = averages[0];
+		System.out.print("Averages:");
+		for (int i = 0; i < averages.length; i++){
 			System.out.print("\t" + averages[i] + "\t");
 			if (averages[i] < minimum){
 				minimum = averages[i];
@@ -93,23 +99,22 @@ public class Project2 {
 			if (averages[i] > maximum){
 				maximum = averages[i];
 			}
-			if(i % 3 == 0 && i != 0){
-				System.out.println("");
-			}
 		}
-
-		System.out.println("Maximum average: " + maximum);
-		System.out.println("Minimum average: " + minimum);
-		if(minimum == maximum){
-			System.out.println("The trials match EXACTLY!");
-		}
-		else if(maximum < (minimum * 2)){
-			System.out.println("The trials concur with each other!");	
-		}
-		else{
-			System.out.println("The trials DO NOT concur with each other!");
-		}
-		in.close();
+		concur(minimum, maximum);
 	}
 
+	public static void concur(int minimum, int maximum){
+		System.out.println("\n\nMinimum average: " + minimum);
+		System.out.println("Maximum average: " + maximum);
+
+		if (minimum == maximum){
+			System.out.println("\nThe trials match EXACTLY!");
+		}
+		else if (maximum < (minimum * 2)){
+			System.out.println("\nThe trials concur with each other!");	
+		}
+		else{
+			System.out.println("\nThe trials DO NOT concur with each other!");
+		}
+	}
 }
